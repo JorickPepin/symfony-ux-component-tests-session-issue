bb79f2ca49475b2e994c01c6a7b599bc7f56af2f
Ben Roberts <ben@headsnet.com>
2023-09-30 07:51:24 +0200

    Live Component tests fail with CSRF accessing the session

---
+++ src/Controller/ExampleLiveComponent.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent('example_live_component')]
+final class ExampleLiveComponent extends AbstractController
+{
+    use DefaultActionTrait;
+
+    // For Live Components, it seems you need to have the
+    // ComponentWithFormTrait and then build a form to see the issue.
+    // The CSRF component is looking for a session which does not exist.
+
+    use ComponentWithFormTrait;
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->createFormBuilder()
+            ->setMethod(Request::METHOD_POST)
+            ->getForm()
+        ;
+    }
+
+    #[LiveAction]
+    public function save(): Response
+    {
+        $this->addFlash('success', 'Saved OK');
+
+        return $this->redirectToRoute('');
+    }
+}
---
+++ tests/Components/ExampleLiveComponentTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Components;
+
+use App\Controller\ExampleLiveComponent;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\UX\LiveComponent\Test\InteractsWithLiveComponents;
+
+class ExampleLiveComponentTest extends KernelTestCase
+{
+    use InteractsWithLiveComponents;
+
+    public function test_can_render_and_interact(): void
+    {
+        $sut = $this->createLiveComponent(
+            name: ExampleLiveComponent::class
+        );
+
+        $this->assertStringContainsString(
+            'This is the example Live Component.',
+            $sut->render()->toString()
+        );
+    }
+}
